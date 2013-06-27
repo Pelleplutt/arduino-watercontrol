7788cf4a46d41bf3129c2d5178a216863145d0f6
Per Lejontand <per.lejontand@gmail.com>
2013-06-27 19:56:18 +0200

    Fix the timestring routinte, this was madness

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -22,23 +22,23 @@ char *timeString(unsigned long seconds) {
     int len = 0;
 
     *buf = 0;
-    if(seconds > 86400) {
+    if(seconds >= 86400) {
         len += snprintf(buf + len, 32 - len, "%dd", seconds / 86400);
         seconds = seconds % 86400;
     }
 
-    if(seconds > 3600) {
+    if(seconds >= 3600) {
         len += snprintf(buf + len, 32 - len, "%dh", seconds / 3600);
         seconds = seconds % 3600;
     }
 
-    if(seconds > 60) {
+    if(seconds >= 60) {
         len += snprintf(buf + len, 32 - len, "%dm", seconds / 60);
         seconds = seconds % 60;
     }
 
     if(seconds % 60) {
-        len += snprintf(buf + len, 32 - len, "%dm", seconds / 60);
+        len += snprintf(buf + len, 32 - len, "%ds", seconds % 60);
     }
 
 
